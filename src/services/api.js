1f704c702cdf88a4b89f04c562880b4be71f2a74
Howard Rosita <howardrosita.hr@gmail.com>
2025-12-06 21:42:17 +0800

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "09ad8ace66eec34302943272db0e8d2c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
